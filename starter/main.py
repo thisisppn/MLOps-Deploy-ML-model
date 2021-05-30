acaec80ee742b682d9a374294cd7f4b76d74d1ad
Partha Pratim Neog <parthapratimneog@Parthas-MacBook-Air.local>
2021-05-31 00:41:46 +0530

    Debugging dvc

--- starter/main.py
+++ starter/main.py
@@ -71,11 +71,17 @@ async def infer(item: Item):
 
 
 if "DYNO" in os.environ and os.path.isdir(".dvc"):
-    os.system("dvc config core.hardlink_lock true")
-    os.system("dvc config core.no_scm true")
-    if os.system("dvc pull") != 0:
-        exit("dvc pull failed")
-    os.system("rm -r .dvc .apt/usr/lib/dvc")
+    try:
+        os.system("dvc config core.no_scm true")
+        os.system("dvc config core.hardlink_lock true")
+
+        os.system("dvc pull")
+        os.system("rm -r .dvc .apt/usr/lib/dvc")
+    except Exception as e:
+        print("Unexpected exception {}".format(e))
+    # if os.system("dvc pull") != 0:
+    #     exit("dvc pull failed")
+    # os.system("rm -r .dvc .apt/usr/lib/dvc")
 
 
 if __name__ == '__main__':
